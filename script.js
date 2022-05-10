06713ba918a24fea0d70623bf003d9840dc393da
Anita Chengalva <anita.chengalva@gmail.com>
2022-05-09 20:41:13 -0700

    var determine which character types to include

--- script.js
+++ script.js
@@ -10,23 +10,31 @@ var specialChar = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", "
 
 // prompt user for password length
 function generatePassword() {
-  var askLength = (prompt("How long would you like your password to be? Input a number of characters between 8-128"));
+  var askLength = prompt("How long would you like your password to be? Input a number of characters between 8-128");
 
   // if user input does not fit bounds for password length,
   // number of characters must be between 8 & 128
-while(askLength < 8 || askLength > 128) {
-  if(askLength < 8){
-    alert("Your input contains too few characters. Please try again.");
-    var askLength = (prompt("How long would you like your password to be? Input a number of characters between 8-128"));
-  }
-  if(askLength > 128){
-    alert("Your input contains too many characters. Please try again.");
-    var askLength = (prompt("How long would you like your password to be? Input a number of characters between 8-128"));
+  while(askLength < 8 || askLength > 128) {
+    if(askLength < 8){
+      alert("Your input contains too few characters. Please try again.");
+      var askLength = (prompt("How long would you like your password to be? Input a number of characters between 8-128"));
+    }
+    if(askLength > 128){
+      alert("Your input contains too many characters. Please try again.");
+      var askLength = (prompt("How long would you like your password to be? Input a number of characters between 8-128"));
+    }
   }
   // confirm character length
   alert(`You chose to have ${askLength} characters`);
+
+  // determine which character types to include
+  var askLowerCase = confirm("Would you like to include lowercase letters in your password?");
+  var askUpperCase = confirm("Would you like to include uppercase letters in your password?");
+  var askNumeric = confirm("Would you like to include numbers in your password?");
+  var askSpecialChar = confirm("Would you like to include special characters in your password?");
 }
-}
+
+
 
 // Write password to the #password input
 function writePassword() {
